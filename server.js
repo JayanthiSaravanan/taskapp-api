e01f7093dafaa8b24839fd389e751b9f390356d9
Jayanthi <jayanthi.saravanann@gmail.com>
2021-01-23 13:50:27 +0530

    removed api word

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('TodoAPI API!'))
+app.get('/', (req, res) => res.send('TodoAPI!'))
 app.listen(port, () => console.log(`Example app listening on port port!`))
