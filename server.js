51230a3ac7c244f94b3a0e4e69c164dd5800ade3
Jayanthi <jayanthi.saravanann@gmail.com>
2021-01-23 14:21:45 +0530

    add API for All task;

--- server.js
+++ server.js
@@ -2,5 +2,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('TodoAPI!'))
+app.get('/', (req, res) => res.send('TodoAPI!'));
+app.get('/alltasks', getAllTasks);
+
+var tasks =[ 
+    {id : 1, name: "task1"}
+];
+
+async function getAllTasks(req,res)
+{
+    res.json(tasks);
+
+}
 app.listen(port, () => console.log(`Example app listening on port port!`))
+
