323efae7b29e9f60c6cc7068724b02da3a4d6893
Jayanthi <jayanthi.saravanann@gmail.com>
2021-01-23 15:06:35 +0530

    search by id

--- server.js
+++ server.js
@@ -9,10 +9,22 @@ var tasks =[
     {id : 1, name: "task1"}
 ];
 
+// list All
 async function getAllTasks(req,res)
 {
     res.json(tasks);
 
 }
+
+//search by tasks by id
+app.get('/tasks/:id',(req,res)=> {
+
+    let id =req.params.id;
+    let task =tasks.find(obj => obj.id==id);
+
+res.send(JSON.stringify(task))
+});
+
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
 
